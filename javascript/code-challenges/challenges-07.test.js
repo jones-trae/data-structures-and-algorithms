72fccd402a3bab3d40f56e3d3eb198c0568c8736
jones-trae <jones.trae@gmail.com>
2021-10-17 11:24:26 -0700

    resub07test

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -154,7 +154,7 @@ Write a function named splitFoods that uses split to produce the same output as 
 
 You may also use other string or array methods.
 ------------------------------------------------------------------------------------------------ */
-
+// change 
 const splitFoods = (recipe) => {
   let result = [];
   // Solution code here...
